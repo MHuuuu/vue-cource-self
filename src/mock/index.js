16a976ea7898fee1a9b48833390a541d1326cea0
Hhuuuu <nothinghu@163.com>
2019-03-19 17:03:30 +0800

    edit-table

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,7 +1,10 @@
 import Mock from 'mockjs'
 import { getUserInfo } from './response/user'
+import { getTableData } from './response/data'
 
 Mock.mock(/\/getUserInfo/, 'post', getUserInfo)
+Mock.mock(/\/getTableData/, 'get', getTableData)
+
 // Mock.mock(/\/getUserInfo/,{name:'zx'})
 
 export default Mock
--- src/router/router.js
+++ src/router/router.js
@@ -1,5 +1,7 @@
 import Home from '@/views/Home.vue'
 import Layout from '@/views/layout.vue'
+import Table from '@/views/table.vue'
+
 export default [
   {
     path: '/',
@@ -9,86 +11,90 @@ export default [
       {
         path: 'home',
         component: Home
+      },
+      {
+        path: 'table',
+        component: Table
       }
     ]
   },
-{
-  path: '/login',
-  name: 'login',
-  component: () => import('@/views/login.vue')
-},
-{
-  path: '/about',
-  name: 'about',
-  // route level code-splitting
-  // this generates a separate chunk (about.[hash].js) for this route
-  // which is lazy-loaded when the route is visited.
-  component: () => import(/* webpackChunkName: "about" */ '@/views/About.vue'),
-  props: {
-    food: 'banana'
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('@/views/login.vue')
   },
-  meta: {
-    title: '关于'
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '@/views/About.vue'),
+    props: {
+      food: 'banana'
+    },
+    meta: {
+      title: '关于'
+    }
+  },
+  {
+    // 动态路由
+    path: '/argu/:name',
+    name: 'argu',
+    component: () => import('@/views/argu.vue'),
+    // 让内部的参数接受外层root的params
+    props: true
+  },
+  {
+    path: '/count-to',
+    name: 'count_to',
+    component: () => import('@/views/count-to.vue')
+  },
+  {
+    path: '/split-pane',
+    name: 'split-pane',
+    component: () => import('@/views/split-pane.vue')
+  },
+  {
+    path: '/render_page',
+    name: 'render_page',
+    component: () => import('@/views/render-page.vue')
+  },
+  {
+    path: '/menu-page',
+    name: 'menu_page',
+    component: () => import('@/views/menu-page.vue')
+  },
+  {
+    // 嵌套路由
+    path: '/parent',
+    name: 'parent',
+    component: () => import('@/views/parent.vue'),
+    children: [{
+      path: 'child',
+      component: () => import('@/views/child.vue')
+    }]
+  },
+  {
+    // 命名视图
+    path: '/named_view',
+    components: {
+      default: () => import('@/views/child.vue'),
+      email: () => import('@/views/email.vue'),
+      tel: () => import('@/views/tel.vue')
+    }
+  },
+  {
+    // 重定向
+    path: '/main',
+    redirect: to => '/'
+  },
+  {
+    path: '/store',
+    component: () => import('@/views/store.vue')
+  },
+  {
+    path: '*',
+    component: () => import('@/views/error_404.vue')
   }
-},
-{
-  // 动态路由
-  path: '/argu/:name',
-  name: 'argu',
-  component: () => import('@/views/argu.vue'),
-  // 让内部的参数接受外层root的params
-  props: true
-},
-{
-  path: '/count-to',
-  name: 'count_to',
-  component: () => import('@/views/count-to.vue')
-},
-{
-  path: '/split-pane',
-  name: 'split-pane',
-  component: () => import('@/views/split-pane.vue')
-},
-{
-  path: '/render_page',
-  name: 'render_page',
-  component: () => import('@/views/render-page.vue')
-},
-{
-  path: '/menu-page',
-  name: 'menu_page',
-  component: () => import('@/views/menu-page.vue')
-},
-{
-  // 嵌套路由
-  path: '/parent',
-  name: 'parent',
-  component: () => import('@/views/parent.vue'),
-  children: [{
-    path: 'child',
-    component: () => import('@/views/child.vue')
-  }]
-},
-{
-  // 命名视图
-  path: '/named_view',
-  components: {
-    default: () => import('@/views/child.vue'),
-    email: () => import('@/views/email.vue'),
-    tel: () => import('@/views/tel.vue')
-  }
-},
-{
-  // 重定向
-  path: '/main',
-  redirect: to => '/'
-},
-{
-  path: '/store',
-  component: () => import('@/views/store.vue')
-},
-{
-  path: '*',
-  component: () => import('@/views/error_404.vue')
-}
 ]
