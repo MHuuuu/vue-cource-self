bc67902ed4a7e04291175ddd8659cc23196b9b43
Hhuuuu <nothinghu@163.com>
2019-03-20 23:09:12 +0800

    folder-tree-2

--- src/views/folder-tree/folder-tree.vue
+++ src/views/folder-tree/folder-tree.vue
@@ -1,34 +1,66 @@
 <template>
   <div class="folder-wrapper">
-    <Tree :data="folderTree" :render="renderFunc"></Tree>
+    <!-- <Tree :data="folderTree" :render="renderFunc"></Tree> -->
+    <folder-tree
+      :folder-list.sync="folderList"
+      :file-list.sync="fileList"
+      :folder-drop="folderDrop"
+      :file-drop="fileDrop"
+      :beforeDelete="beforeDelete"
+    />
   </div>
 </template>
 
 <script>
 import { getFolderList, getFileList } from '@/api/data'
-import { putFileInFolder, transferFolderToTree } from '@/lib/util'
+import FolderTree from '_c/folder-tree'
 export default {
+  components: {
+    FolderTree
+  },
   data () {
     return {
       folderList: [],
       fileList: [],
-      folderTree: [],
-      renderFunc: (h, { root, node, data }) => {
-        return (
-          <div class="tree-item">
-            { data.type === 'folder' ? <icon type="ios-folder" color="#2d8cf0" style="margin-right: 10px;"/> : ''}
-            { data.title }
-          </div>
-        )
-      }
+      folderDrop: [
+        {
+          name: 'rename',
+          title: '重命名'
+        },
+        {
+          name: 'delete',
+          title: '删除文件夹'
+        }
+      ],
+      fileDrop: [
+        {
+          name: 'rename',
+          title: '重命名'
+        },
+        {
+          name: 'delete',
+          title: '删除文件'
+        }
+      ]
+    }
+  },
+  methods: {
+    beforeDelete () {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          // let error = new Error('error')
+          let error = null
+          if (!error) {
+            resolve()
+          } else reject(error)
+        }, 2000)
+      })
     }
   },
   mounted () {
     Promise.all([getFolderList(), getFileList()]).then(res => {
-      console.log(res[0], res[1]);
-      console.log(putFileInFolder(res[0], res[1]));
-      this.folderTree = transferFolderToTree(putFileInFolder(res[0], res[1]))
-      //console.log('文件树',this.folderTree);
+      this.folderList = res[0]
+      this.fileList = res[1]
     })
   }
 }
@@ -37,11 +69,5 @@ export default {
 <style lang="less">
 .folder-wrapper{
   width: 300px;
-  .tree-item{
-    display: inline-block;
-    width: ~"calc(100% - 50px)";
-    height: 30px;
-    line-height: 30px;
-  }
 }
 </style>
